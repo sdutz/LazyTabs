f9ce7153ef9a8c99008c4caf6096de48b13ba5b1
sdutz <zambellilorenzo@gmail.com>
2018-05-12 22:12:43 +0200

    Completed keyboard draw

--- qguitarkeyboard.h
+++ qguitarkeyboard.h
@@ -5,12 +5,17 @@
 class QGuitarKeyboard {
     public:
         explicit QGuitarKeyboard();
+        explicit QGuitarKeyboard( int nStrings, int nFrets, int nStringDst, int nFretsDst);
         bool Draw( QGraphicsScene* pScene) ;
 
     protected:
-        void Init() ;
+        void Init( void) ;
 
     private:
+        int          m_nFrets ;
+        int          m_nStrings ;
+        int          m_nFretDst ;
+        int          m_nStringDst ;
         QVector<int> m_anFrets ;
         QVector<int> m_anStrings ;
 };
