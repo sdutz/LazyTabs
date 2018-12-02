93340034924a5806fe89a046b46ef3d5a287b485
sdutz <zambellilorenzo@gmail.com>
2018-12-02 08:12:58 +0100

    Added translations files

---
+++ LazyTabs_en.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="about.ui" line="26"/>
+        <source>Dialog</source>
+        <translation>About</translation>
+    </message>
+</context>
+<context>
+    <name>LazyTabsDlg</name>
+    <message>
+        <location filename="lazytabsdlg.ui" line="26"/>
+        <source>LazyTabs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="52"/>
+        <source>Add chord to list</source>
+        <translation>Add chord to list</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="81"/>
+        <source>Send song to clipboard</source>
+        <translation>Send song to clipboard</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="120"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="149"/>
+        <source>Shift to left</source>
+        <translation>Shift to left</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="178"/>
+        <source>Shift to right</source>
+        <translation>Shift to right</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="207"/>
+        <source>Reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="236"/>
+        <source>Insert chord by name</source>
+        <translation>Insert chord</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="265"/>
+        <source>Load</source>
+        <translation>Load</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="294"/>
+        <location filename="lazytabsdlg.cpp" line="224"/>
+        <location filename="lazytabsdlg.cpp" line="302"/>
+        <location filename="lazytabsdlg.cpp" line="332"/>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="323"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="352"/>
+        <source>New</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="381"/>
+        <source>Delete last chord from list</source>
+        <translation>Delete last chord from list</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="148"/>
+        <source>Type your chord</source>
+        <translation>Type your chord</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="148"/>
+        <source>eg : D#+</source>
+        <translatorcomment>eg : D#+</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="224"/>
+        <location filename="lazytabsdlg.cpp" line="302"/>
+        <location filename="lazytabsdlg.cpp" line="332"/>
+        <source>Do you want to save your progress?</source>
+        <translation>Do you want to save your progress?</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="232"/>
+        <location filename="lazytabsdlg.cpp" line="274"/>
+        <source>Select project file</source>
+        <translation>Select project file</translation>
+    </message>
+</context>
+<context>
+    <name>TabsScene</name>
+    <message>
+        <location filename="tabsscene.cpp" line="334"/>
+        <source>Warning</source>
+        <translation>Warning</translation>
+    </message>
+    <message>
+        <location filename="tabsscene.cpp" line="335"/>
+        <source>this cord will stretch your hand, are you sure it&apos;s correct?</source>
+        <translation>This cord will stretch your hand, are you sure it&apos;s correct?</translation>
+    </message>
+</context>
+<context>
+    <name>settingsdlg</name>
+    <message>
+        <location filename="settingsdlg.ui" line="26"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="77"/>
+        <source>Strings</source>
+        <translation>Strings</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="109"/>
+        <source>Frets</source>
+        <translation>Frets</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="132"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="145"/>
+        <source>Database</source>
+        <translation>Database</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="168"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.cpp" line="40"/>
+        <source>Select db file</source>
+        <translation>Select db file</translation>
+    </message>
+</context>
+</TS>
---
+++ LazyTabs_it.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="about.ui" line="26"/>
+        <source>Dialog</source>
+        <translation>Informazioni</translation>
+    </message>
+</context>
+<context>
+    <name>LazyTabsDlg</name>
+    <message>
+        <location filename="lazytabsdlg.ui" line="26"/>
+        <source>LazyTabs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="52"/>
+        <source>Add chord to list</source>
+        <translation>Aggiungi accordo alla lista</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="81"/>
+        <source>Send song to clipboard</source>
+        <translation>Copia canzone in copia</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="120"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="149"/>
+        <source>Shift to left</source>
+        <translation>Muovi a sinistra</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="178"/>
+        <source>Shift to right</source>
+        <translation>Muovi a destra</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="207"/>
+        <source>Reset</source>
+        <translation>Resetta</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="236"/>
+        <source>Insert chord by name</source>
+        <translation>Inserisci accordo</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="265"/>
+        <source>Load</source>
+        <translation>Carica</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="294"/>
+        <location filename="lazytabsdlg.cpp" line="224"/>
+        <location filename="lazytabsdlg.cpp" line="302"/>
+        <location filename="lazytabsdlg.cpp" line="332"/>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="323"/>
+        <source>Help</source>
+        <translation>Aiuto</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="352"/>
+        <source>New</source>
+        <translation>Nuovo</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.ui" line="381"/>
+        <source>Delete last chord from list</source>
+        <translation>Cancella ultimo accordo dalla lista</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="148"/>
+        <source>Type your chord</source>
+        <translation>Digita il tuo accordo</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="148"/>
+        <source>eg : D#+</source>
+        <translatorcomment>Esempio : D#+</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="224"/>
+        <location filename="lazytabsdlg.cpp" line="302"/>
+        <location filename="lazytabsdlg.cpp" line="332"/>
+        <source>Do you want to save your progress?</source>
+        <translation>Vuoi salvare i tuoi progressi?</translation>
+    </message>
+    <message>
+        <location filename="lazytabsdlg.cpp" line="232"/>
+        <location filename="lazytabsdlg.cpp" line="274"/>
+        <source>Select project file</source>
+        <translation>Seleziona file di progetto</translation>
+    </message>
+</context>
+<context>
+    <name>TabsScene</name>
+    <message>
+        <location filename="tabsscene.cpp" line="334"/>
+        <source>Warning</source>
+        <translation>Avviso</translation>
+    </message>
+    <message>
+        <location filename="tabsscene.cpp" line="335"/>
+        <source>this cord will stretch your hand, are you sure it&apos;s correct?</source>
+        <translation>Questo accordo sarà faticoso per la tua mano, sicuro sia corretto?</translation>
+    </message>
+</context>
+<context>
+    <name>settingsdlg</name>
+    <message>
+        <location filename="settingsdlg.ui" line="26"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="77"/>
+        <source>Strings</source>
+        <translation>Corde</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="109"/>
+        <source>Frets</source>
+        <translation>Tasti</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="132"/>
+        <source>Language</source>
+        <translation>Lingua</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="145"/>
+        <source>Database</source>
+        <translation>Databse</translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.ui" line="168"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdlg.cpp" line="40"/>
+        <source>Select db file</source>
+        <translation>Seleziona il file db</translation>
+    </message>
+</context>
+</TS>
